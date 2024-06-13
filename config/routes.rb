3be720a5f30a4019b5e0b53b7a038d5e191f88b2
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-06-13 14:54:52 +0000

    [add]headerに必要なリンクを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'users/index'
-  resource :users, only:[:show, :edit, :index]
-  resource :books, only:[:show, :edit, :index]
+  resources :users, only:[:show, :edit, :index]
+  resources :books, only:[:show, :edit, :index]
   devise_for :users
   root to: "homes#top"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html

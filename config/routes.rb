7fdacfbfe89ffb76024dbc567cbd1eeb848cd1da
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-06-14 09:00:23 +0000

    [add]log inページの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only:[:show, :edit, :index]
-  resources :books, only:[:show, :edit, :index]
   devise_for :users
   root to: "homes#top"
+  get '/homes/about' => 'homes#about', as: 'about'
+
+
+  resources :users, only:[:show, :edit, :index]
+  resources :books, only:[:show, :edit, :index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

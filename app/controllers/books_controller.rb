52152706388aa96d2b55cc285d75b040b37a41d8
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-06-24 10:29:23 +0000

    [add]bookのindexページの右側の作成(Books)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,32 @@
 class BooksController < ApplicationController
   def show
     @book = Book.new
+    @user = current_user
   end
 
   def edit
   end
 
   def index
-    @books = Book.all
+    @book = Book.new
     @user = current_user
+    @books = Book.all
   end
 
   def create
-    @book = Book.new
+    @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
       redirect_to root_path
     else
-      render :index
+      render :show
     end
   end
 
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   has_one_attached :image
 
   has_many :books, dependent: :destroy
+
+  def get_profile_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/default-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
 end

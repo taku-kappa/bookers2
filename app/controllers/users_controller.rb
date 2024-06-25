c64150ba317df033f8d887d2e96fdd2adad98582
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-06-25 15:31:26 +0000

    [add]userのupdate機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @book = Book.new
     @user = current_user
     user = User.find(params[:id])
-    @books.user =
+    @books = user.books
   end
 
   def create
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def index
@@ -25,10 +26,20 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def update
+    user = User.find(params[:id])
+    user.update(user_params)
+    redirect_to user_path(user.id)
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image)
   end
 
 end

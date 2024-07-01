5148ad010f38633efaf0df4cfea40db87bf26742
taku-kappa <168840294+taku-kappa@users.noreply.github.com>
2024-07-01 09:19:16 +0000

    [add]Rspecテスト後修正（仕上げのテスト グリッドシステムのテスト: container, row, col-md-〇を正しく使えている ユーザ一覧画の手前まで）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,7 +45,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :introduction, :image)
+      params.require(:user).permit(:name, :introduction, :profile_image)
     end
 
     def is_matching_login_user
